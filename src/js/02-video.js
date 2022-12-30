9f71c5f3365bd579f48f5b45d250451fc32ff221
Ivan Osadchuk <osadchukit@gmail.com>
2022-12-30 17:17:45 +0100

    player finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+// const KEY_VIDEOPLAYER = 'videoplayer-current-time';
+
+// const onPlay = function (data) {
+//     localStorage.setItem(KEY_VIDEOPLAYER, data.seconds);
+// };
+
+// player.on('timeupdate', onPlay);
+
+// let time = localStorage.getItem(KEY_VIDEOPLAYER) || 0;
+// // console.log(time);
+
+// player.setCurrentTime(time)
+
+// --------------------
+
+player.on('timeupdate', data =>
+  localStorage.setItem('videoplayer-current-time', data.seconds)
+);
+
+let time = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(time || 0);
