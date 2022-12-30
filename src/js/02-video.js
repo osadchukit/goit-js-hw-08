ee6a5d71f84b2e3e5c7e7dcb7a0080338af3f374
Ivan Osadchuk <osadchukit@gmail.com>
2022-12-30 23:09:30 +0100

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,29 @@
-
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-// const KEY_VIDEOPLAYER = 'videoplayer-current-time';
+const player = new Player(iframe);
+const KEY_VIDEOPLAYER = 'videoplayer-current-time';
 
-// const onPlay = function (data) {
-//     localStorage.setItem(KEY_VIDEOPLAYER, data.seconds);
-// };
+const onPlay = function (data) {
+  localStorage.setItem(KEY_VIDEOPLAYER, data.seconds);
+};
 
-// player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-// let time = localStorage.getItem(KEY_VIDEOPLAYER) || 0;
-// // console.log(time);
+let time = localStorage.getItem(KEY_VIDEOPLAYER) || 0;
+// console.log(time);
 
-// player.setCurrentTime(time)
+player.setCurrentTime(time);
 
 // --------------------
 
-player.on('timeupdate', data =>
-  localStorage.setItem('videoplayer-current-time', data.seconds)
-);
+// player.on(
+//   'timeupdate',
+//   throttle(
+//     data => localStorage.setItem('videoplayer-current-time', data.seconds),
+//     1000
+//   )
+// );
 
-let time = localStorage.getItem('videoplayer-current-time');
-
-player.setCurrentTime(time || 0);
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
